9d8134233dda330cf888242a3d30cbe1253ea24c
yxlin <yishinlin001@gmail.com>
2024-07-07 09:04:44 +0800

    update to a new version.

---
+++ app/CV/page.tsx
@@ -0,0 +1,18 @@
+import { BlogPosts } from 'app/components/posts'
+
+export const metadata = {
+  title: 'Résumé',
+  description: 'Read my résumé.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      {/* <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My CV</h1> */}
+      <h1> EXPERIENCE </h1>
+      <h2>MARCH 2020 – SEPTEMBER 2023 </h2>
+      RESEARCH FELLOW, UNIVERSITY OF LEEDS
+
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,29 +7,34 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        Welcome to my personal website! I am an experimental and cognitive psychologist with expertise in statistics and mathematical modeling.
-        The mathematical models I've developed include machine learning models and cognitive decision models. These models have been applied to:
+        Welcome! I am an experimental psychologist with expertise in statistics and 
+        computational modeling. The models I've developed include machine learning 
+        models and cognitive models. They have been applied to:
       </p>
+
       <p>
         <ul>
-        <li>Understanding individual cognitive processes;</li>
-        <li>Exploring social group cognition;</li>
-        <li>Studying rational and irrational behavior in human economic decisions;</li>
-        <li>Designing safe pedestrian traffic environments;</li>
-        <li>Addressing ethical choices at intersections.</li>
+          <li>1. Understanding individual cognitive processes;</li>
+          <li>2. Exploring social group cognition;</li>
+          <li>3. Studying irrational behaviors in human decisions;</li>
+          <li>4. Designing safe pedestrian traffic environments;</li>
+          <li>5. Addressing ethical choices at intersections.</li>
         </ul>
       </p>
+
       <p>
-        My research involves applying hierarchical Bayesian inference and 
-        high-speed CUDA-GPU computation to understand human decision-making 
-        processes and visual search mechanisms. For example, I've used machine 
-        learning models to analyze pedestrian behavior at crosswalks and study 
-        driver decision-making during lane changes on highways. Successful 
-        applications of this research include using decision tree models to 
-        automatically extract interactions between pedestrians and vehicles 
-        from CCTV traffic footage, identifying potential traffic accidents 
+      <br/>
+        My research involves applying hierarchical Bayesian inference and
+        high-speed CUDA-GPU computation to understand human decision-making
+        processes and visual search mechanisms. For example, I've used machine
+        learning models to analyze pedestrian behavior at crosswalks and study
+        driver decision-making during lane changes on highways. Successful
+        applications of this research include using decision tree models to
+        automatically extract interactions between pedestrians and vehicles
+        from CCTV traffic footage, identifying potential traffic accidents
         and conflicts.
       </p>
+
       <div className="my-8">
         <BlogPosts />
       </div>
